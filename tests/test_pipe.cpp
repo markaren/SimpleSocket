5085582cadf06131acc0366c8f86f731ba3ef9c8
Lars Ivar Hatledal <laht@ntnu.no>
2024-09-24 14:53:18 +0200

    add test

---
+++ tests/test_pipe.cpp
@@ -0,0 +1,66 @@
+
+#include "simple_socket/Pipe.hpp"
+
+#include <thread>
+#include <vector>
+
+#include <catch2/catch_test_macros.hpp>
+
+using namespace simple_socket;
+
+namespace {
+
+    const std::string domain{"test_pipe"};
+
+    std::string generateMessage() {
+
+        return "Per";
+    }
+
+    std::string generateResponse(const std::string& msg) {
+
+        return "Hello " + msg + "!";
+    }
+
+}// namespace
+
+TEST_CASE("NamedPipe read/write") {
+
+    std::thread serverThread([] {
+        std::unique_ptr<NamedPipe> conn = NamedPipe::listen(domain);
+        REQUIRE(conn);
+
+        std::vector<unsigned char> buffer(1024);
+        const auto bytesRead = conn->receive(buffer);
+
+        std::string expectedMessage = generateMessage();
+        REQUIRE(bytesRead == expectedMessage.size());
+
+        std::string msg(buffer.begin(), buffer.begin() + bytesRead);
+        REQUIRE(msg == expectedMessage);
+
+        REQUIRE(conn->send(generateResponse(msg)));
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    std::thread clientThread([] {
+        const auto conn = NamedPipe::connect(domain);
+        REQUIRE(conn);
+
+        std::string message = generateMessage();
+        std::string expectedResponse = generateResponse(message);
+
+        conn->send(message);
+
+        std::vector<unsigned char> buffer(1024);
+        const auto bytesRead = conn->receive(buffer);
+        REQUIRE(bytesRead == expectedResponse.size());
+        std::string response(buffer.begin(), buffer.begin() + bytesRead);
+
+        CHECK(response == expectedResponse);
+    });
+
+    clientThread.join();
+    serverThread.join();
+}
