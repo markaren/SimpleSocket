d899a60ab7af377b66a30aead5789771dda887cb
Lars Ivar Hatledal <laht@ntnu.no>
2024-09-25 15:12:59 +0200

    update ws demo

--- tests/integration/run_ws.cpp
+++ tests/integration/run_ws.cpp
@@ -8,15 +8,15 @@ using namespace simple_socket;
 int main() {
 
     WebSocket ws(8081);
-    ws.onOpen = [](auto) {
-        std::cout << "onOpen" << std::endl;
+    ws.onOpen = [](auto c) {
+        std::cout << "[" << c->uuid() << "] onOpen" << std::endl;
     };
-    ws.onClose = [](auto) {
-        std::cout << "onClose" << std::endl;
+    ws.onClose = [](auto c) {
+        std::cout << "[" << c->uuid() << "] onClose" << std::endl;
     };
     ws.onMessage = [](auto c, const auto& msg) {
-        std::cout << "onMessage: " << msg << std::endl;
-        c->send("Hello from server");
+        std::cout << "[" << c->uuid() << "] onMessage: " << msg << std::endl;
+        c->send(msg);
     };
 
     system("start ws_client.html");
