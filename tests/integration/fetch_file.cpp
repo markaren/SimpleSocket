7cd4d66a445a63ae0354aa433a8adb42f914df54
Lars Ivar Hatledal <laht@ntnu.no>
2025-10-21 13:07:08 +0200

    Add SimpleHTTPFetcher

---
+++ tests/integration/fetch_file.cpp
@@ -0,0 +1,20 @@
+
+#include "simple_socket/SimpleHttpFetcher.hpp"
+
+#include <fstream>
+#include <iostream>
+
+using namespace simple_socket;
+
+int main() {
+    SimpleHttpFetcher fetcher;
+    const auto response = fetcher.fetch("https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg");
+
+    if (response) {
+        std::cout << response.value() << std::endl;
+        std::ofstream output("Placeholder_view_vector.svg", std::ios::binary);
+        output << *response;
+    } else {
+        std::cerr << "Failed to fetch the file." << std::endl;
+    }
+}
